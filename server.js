c510b71659aa1dfe5b31e6a03e7cc8548bea1bb1
Chris Arrington <carrington@bandwidth.com>
2021-05-07 17:02:57 -0400

    EDGE-848 Add check for required variables, make env.default consistent w/ TS version

--- server.js
+++ server.js
@@ -12,8 +12,10 @@ app.use(bodyParser.json());
 app.use(express.static("public"));
 
 // config
-const port = 3000;
+const port = process.env.PORT || 3000;
 const accountId = process.env.BW_ACCOUNT_ID;
+const username = process.env.BW_USERNAME;
+const password = process.env.BW_PASSWORD;
 
 // Global variables
 BandwidthWebRTC.Configuration.basicAuthUserName = process.env.BW_USERNAME;
@@ -31,6 +33,14 @@ let calls = new Map();
 //  - if not a demo, these would be stored in persistant storage
 let sessionId = false;
 let callId = false;
+
+// Check to make sure required environment variables are set
+if (!accountId || !username || !password) {
+  console.error(
+      "ERROR! Please set the BW_ACCOUNT_ID, BW_USERNAME, and BW_PASSWORD environment variables before running this app"
+  );
+  process.exit(1);
+}
 
 /**
  * Setup the call and pass info to the browser so they can join
@@ -246,7 +256,7 @@ async function initiateCallToPSTN(account_id, from_number, to_number) {
     from: from_number,
     to: to_number,
     applicationId: process.env.BW_VOICE_APPLICATION_ID,
-    answerUrl: process.env.BASE_CALLBACK_URL + "callAnswered",
+    answerUrl: process.env.BASE_CALLBACK_URL + "/callAnswered",
     answerMethod: "POST",
     callTimeout: "30",
   });
