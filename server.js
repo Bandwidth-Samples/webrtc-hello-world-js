3976b63071b4f39597a2338692780e5b96bdaa7a
carrington-bw <70553757+carrington-bw@users.noreply.github.com>
2021-05-10 09:15:41 -0400

    EDGE-848 Update to 0.9.x Browser SDK, V3 Participants (#4)
    * EDGE-848 Update to 0.9.x Browser SDK, V3 Participants
    
    * EDGE-848 Add check for required variables, make env.default consistent w/ TS version

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
-const fs = require("fs");
 const express = require("express");
 const BandwidthWebRTC = require("@bandwidth/webrtc");
 const BandwidthVoice = require("@bandwidth/voice");
 const uuid = require("uuid");
-const dotenv = require("dotenv").config();
-const jwt_decode = require("jwt-decode");
+const dotenv = require("dotenv");
 const app = express();
 const bodyParser = require("body-parser");
+
+dotenv.config();
+
 app.use(bodyParser.json());
 app.use(express.static("public"));
 
 // config
-const port = 3000;
-const localDir = __dirname;
+const port = process.env.PORT || 3000;
 const accountId = process.env.BW_ACCOUNT_ID;
+const username = process.env.BW_USERNAME;
+const password = process.env.BW_PASSWORD;
 
 // Global variables
 BandwidthWebRTC.Configuration.basicAuthUserName = process.env.BW_USERNAME;
@@ -32,6 +34,14 @@ let calls = new Map();
 let sessionId = false;
 let callId = false;
 
+// Check to make sure required environment variables are set
+if (!accountId || !username || !password) {
+  console.error(
+      "ERROR! Please set the BW_ACCOUNT_ID, BW_USERNAME, and BW_PASSWORD environment variables before running this app"
+  );
+  process.exit(1);
+}
+
 /**
  * Setup the call and pass info to the browser so they can join
  */
@@ -46,7 +56,7 @@ app.get("/startBrowserCall", async (req, res) => {
     await addParticipantToSession(accountId, participant.id, session_id);
     // now that we have added them to the session, we can send back the token they need to join
     res.send({
-      message: "created particpant and setup session",
+      message: "created participant and setup session",
       token: token,
     });
   } catch (error) {
@@ -193,6 +203,7 @@ async function createParticipant(account_id, tag) {
   var participantBody = new BandwidthWebRTC.Participant({
     tag: tag,
     publishPermissions: ["AUDIO"],
+    deviceApiVersion: "V3"
   });
 
   try {
@@ -245,7 +256,7 @@ async function initiateCallToPSTN(account_id, from_number, to_number) {
     from: from_number,
     to: to_number,
     applicationId: process.env.BW_VOICE_APPLICATION_ID,
-    answerUrl: process.env.BASE_CALLBACK_URL + "callAnswered",
+    answerUrl: process.env.BASE_CALLBACK_URL + "/callAnswered",
     answerMethod: "POST",
     callTimeout: "30",
   });
